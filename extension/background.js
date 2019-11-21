c07585d05f86768fa4263a5115910393fe6fc77d
andrew <andrewfelder95@gmail.com>
2019-11-21 17:51:40 -0600

    added user set and lots of comments

--- extension/background.js
+++ extension/background.js
@@ -1,8 +1,31 @@
 var websiteMap = {}; //Domains --> list of extensions that are blocked
+
+// websiteMap serves as a nested dictionary {website : {[extensionID: true/false, extensionID: true/false ...]}}
+// in this case, the primary key is the website. Each website then has a key
+// for all extensions in the browser which have a value of true or false.
+// true indicates that the extension is allowed, false indicates that the exetension is blocked.
+
+// For example, I used the userset mode and blocked 3 extensions on paypal.com
+// this is the resulting structure:
+
+// {paypal.com: {…}}
+//  -> paypal.com:
+//      -> aapbdbdomjkkjkaonfhkkikfgjllcleb: false
+//      -> bmnlcjabgnpnenekpadlanbbkooimhnj: false
+//      -> cjpalhdlnbpafiamejdnhcphjbkeiagm: true
+//      -> eekbbmglbfldjpgbmajenafphnfjonnc: false
+//      -> eimadpbcbfnmbkopoojfekhnkhdbieeh: true
+
+// the parent, {paypal.com: {...}} is just the object, it doesn't show any data
+// expanding it shows the key 'paypal.com' which then has a list of sub items
+// each item consists of the ID of an extension as well as whether or not it is
+// blocked
+
+
 var extensionsList;
 var mode = "flexible";
 
-//Top 50 banking websites
+//Top 50 banking websites - only used in Sensitive mode
 var sensitiveWebsites = ["paypal.com", "chase.com", "bankofamerica.com", "wellsfargo.com", "americanexpress.com", "hdfcbank.com",
     "capitalone.com", "icicibank.com", "discover.com", "td.com", "usbank.com", "commbank.com.au", "axisbank.com", "scotiabank.com",
     "chase.com", "rbcroyalbank.com", "fnb.co.za", "hangseng.com", "bmo.com", "cibc.com", "tdbank.com", "anz.com", "nab.com.au",
@@ -11,35 +34,68 @@ var sensitiveWebsites = ["paypal.com", "chase.com", "bankofamerica.com", "wellsf
     "bankofindia.com", "nwolb.com", "online.citibank.co.in", "anz.com.au", "absa.co.za", "yesbank.in", "hsbc.com", "bsi.ir",
     "aib.ie", "standardbank.co.za", "becu.org"];
 
+//websites and their extensions defined by the user in user set mode
+var usersetWebsites = {}
+
+
+// var usersetBlockedExtensions = {}
 
 function getExtensionInfo(){
     return extensionsList;
 }
 
+// checks to see if site is a key entry in websiteMap
 function hasWebsiteSettings(site){
     return websiteMap.hasOwnProperty(site);
 }
 
+//returns the value corresponding the the given key for websiteMap
 function getWebsiteSettings(site){
     return websiteMap[site];
 }
 
+// I believe this one is only used when you turn extensions on/off manually
+// through their extension. Basically, it takes the new settings for the site
+// (e.g., you're on youtube and turn off ad blocker, that setting is now false
+// to symbolize the extension being blocked) and updates the existing settings
+
+//chrome.storage.sync is an account wide storage so you can access it on
+// multiple devices provided the same google account is logged in
+// there's also chrome.storage.local which is specific to that device
 function saveWebsiteSettings(site,settings){
     //Save the current policy to the map
     websiteMap[site] = settings;
-
+    if(mode == "userset"){
+      var blocked = []
+      for (var i in settings){
+        // if the extension is blocked
+        if(settings[i] == false){
+          blocked.push(i);
+        }
+      }
+      // update the newly blocked or unblocked extensions
+      usersetWebsites[site] = blocked;
+    }
     //Refresh policies
     chrome.storage.sync.clear();
     chrome.storage.sync.set(mapToSettings());
 }
 
 // Function to transform the website map to ExtensionSettings
+// this functions modifies the actual extensions' settings
 function mapToSettings(){
     let settings = {};
     if(mode !== "strict") {
+        // set all extensions to not have any blocked hosts by default
+        // i.e. each extension's settings is given an empty array of blocked hosts
         for(let index in extensionsList) {
             settings[extensionsList[index].id] = {"runtime_blocked_hosts": []};
         }
+        // for each website in the map and each extension
+        // for each extension's id and whether or not it's allowed
+        // block extensions which are not allowed by adding that website to
+        // the extension's list of blocked hosts
+        // this updates the settings of the extensions
         for (let [website, extensions] of Object.entries(websiteMap)) {
             for (let [id, allowed] of Object.entries(extensions)) {
                 if (!allowed) {
@@ -47,7 +103,11 @@ function mapToSettings(){
                 }
             }
         }
-    } else {
+    }
+    // opposite if strict mode. Set blocked hosts to be everything and allowed
+    // hosts to be empty. If the extension is allowed by the user (this has to
+    // be manually turned on by the user while on that website) add it to allowed
+    else {
         for(let index in extensionsList) {
             settings[extensionsList[index].id] = {"runtime_blocked_hosts": ["*://*"], "runtime_allowed_hosts": []};
         }
@@ -63,6 +123,8 @@ function mapToSettings(){
 }
 
 //Function to transform the stored ExtensionSettings to a website map
+// this is the opposite of the above function. It takes the extensions' settings
+// and populates the websiteMap variable.
 function settingsToMap(settings){
 
     let ids = [];
@@ -120,14 +182,20 @@ function refreshExtensionList() {
         //Remove this extension from the list
         for(let index in extensionsList){
             if(extensionsList[index].id == chrome.runtime.id){
+              // splice(i, x) removes x values from an array starting at index i
                 extensionsList.splice( index, 1 );
             }
         }
-
+        // get the user's extension settings and populate the websiteMap on startup
         chrome.storage.sync.get(['ExtensionSettings'], function(result) {
             websiteMap = settingsToMap(result['ExtensionSettings']);
         });
 
+    });
+
+    //  this should probably be moved elsewhere but it works well enough here for now
+    chrome.storage.sync.get(['websites'], function(results){
+      usersetWebsites = results.websites
     });
 }
 
@@ -170,39 +238,61 @@ chrome.runtime.onMessage.addListener(
 
 
 //Functions to handle mode change
+// flexible mode allows all extensions by default and requires the user to manually
+// disable them on chosen websites
 function flexibleMode(){
     //We reset the map
     //resetMap();
 
-    //We remove the preloaded list
+    //We remove the preloaded lists
     removePreloadListFromBlacklist();
+    removeUserSet();
 
     //We save the new mode and we refresh the policy
     saveMode("flexible");
 }
 
+// sensitive mode blocks all extensions by default on the websites listed in the
+// 'sensitiveWebsites' variable defined at the top. All other websites have all
+// extensions enabled by default.
 function sensitiveMode(){
     //We reset the map
     //resetMap();
 
-    //We add the preloaded list
+    // remove the user set list and add the sensitive list to websiteMap
+    removeUserSet();
     addPreloadListFromBlacklist();
 
     //We save the new mode and we refresh the policy
     saveMode("sensitive");
 }
 
+// strict mode blocks all extensions everywhere by default. Extensions must be
+// manually turned on by the user
 function strictMode(){
     //We reset the map
     //resetMap();
 
-    //We remove the preloaded list
+    //We remove the preloaded lists
+    removeUserSet();
     removePreloadListFromBlacklist();
 
     //We save the new mode and we refresh the policy
     saveMode("strict");
 }
 
+// this is the one I made which is a middle point between flexible and strict
+// where the user can kind of build their own website list and blocked extensions
+// without having to visit those websites and do it manually
+function userSetMode(){
+  // remove the sensitive list and add the user list to websiteMap
+  removePreloadListFromBlacklist();
+  preloadUserSet();
+  saveMode("userset")
+}
+
+// load all websites in from the sensitiveWebsites variable and set all extension
+// ids to have a value of false
 function addPreloadListFromBlacklist(){
     for(let i in sensitiveWebsites){
 
@@ -218,12 +308,95 @@ function addPreloadListFromBlacklist(){
     }
 }
 
+
+// clear the website map after switching from sensitive to another mode
 function removePreloadListFromBlacklist(){
     for(let i in sensitiveWebsites){
         delete websiteMap[sensitiveWebsites[i]];
     }
 }
 
+// basically the same as the sensitive website loading function
+function preloadUserSet(){
+  for (var site in usersetWebsites){
+    if(usersetWebsites.hasOwnProperty(site)){
+      var value = usersetWebsites[site];
+      // alert(value)
+      if(!websiteMap.hasOwnProperty(site)){
+          websiteMap[site] = {};
+      }
+      // set unblocked websites to be true so they show up as enabled if the user
+      // clicks this extension's icon and brings up the dropdown on that website
+      for( var ext in extensionsList){
+        websiteMap[site][extensionsList[ext].id] = true;
+      }
+      for(var id in value){
+        websiteMap[site][value[id]] = false;
+      }
+    }
+  }
+}
+
+function removeUserSet(){
+  for(let i in usersetWebsites){
+    delete websiteMap[i]
+  }
+}
+
+// used when the user adds a website to their blacklist for user set mode
+function addToUserSetBlacklist(url, extensions){
+  if(mode == "userset"){
+    // start by clearing the set, but only if we're in userset mode
+    // if we're not in user set mode, we want to leave the websiteMap alone
+    // this function still works while not in userset mode
+      removeUserSet();
+  }
+  // deep copy
+  e = JSON.parse(JSON.stringify(extensions))
+  // this function is called from options.js, so that majority of the work happens in
+  // there. Here it just does assignment
+  usersetWebsites[url] = e
+  // store the new settings for whenever they need to be reloaded
+  chrome.storage.sync.set({'websites': usersetWebsites})
+  // if we're in userset mode currently, go ahead and reload the settings
+  // preloadUserSet puts the user defined websites into websiteMap
+  // chrome calls actually apply the settings
+  if(mode == "userset"){
+    preloadUserSet();
+    //Refresh policies
+    chrome.storage.sync.clear();
+    chrome.storage.sync.set(mapToSettings());
+  }
+  // return usersetWebsites;
+}
+
+// same thing as previous function, but remove instead of add
+function removeFromUserSetBlacklist(url){
+  if(mode == "userset"){
+      removeUserSet();
+  }
+  delete usersetWebsites[url];
+  chrome.storage.sync.set({'websites': usersetWebsites})
+  if(mode == "userset"){
+    preloadUserSet();
+    //Refresh policies
+    chrome.storage.sync.clear();
+    chrome.storage.sync.set(mapToSettings());
+  }
+
+}
+
+// clears the user set
+function resetUserBlacklist(){
+  if(mode == "userset"){
+    removeUserSet();
+  }
+  usersetWebsites = {};
+  chrome.storage.sync.set({'websites': usersetWebsites})
+}
+
+
+// clears the website map
 function resetMap(){
     websiteMap = {};
     chrome.storage.sync.clear();
@@ -232,8 +405,9 @@ function resetMap(){
 
 
 // Initialisation
+// not sure what the listeners do, but the refreshExtensionList call should happen
+// every time a page is loaded?
 chrome.management.onInstalled.addListener(refreshExtensionList);
 chrome.management.onUninstalled.addListener(refreshExtensionList);
 refreshExtensionList();
 initMode();
-
--- extension/options.js
+++ extension/options.js
@@ -1,13 +1,26 @@
-var background = chrome.extension.getBackgroundPage();
 
+//basically loads the background.js file. Allows you to call background.js
+// functions and variables using background.Function or background.Variable
+var background = chrome.extension.getBackgroundPage();
+// console.log(background)
+
+var userExtensionList = []
+
+// loads when the options page is loaded
+// basically presets html stuff
 function restore_options() {
   chrome.storage.local.get({
     mode: 'flexible',
   }, function(items) {
     document.getElementById("selectMode").value = items.mode;
   });
+
+  listExtensions(background.extensionsList);
+  document.getElementById("extensionList").addEventListener("click", modifyUserExtensionList);
+
 }
 
+// calls the background function to actually change mode when the user changes mode
 function changeMode(mode) {
     console.log("changeMode");
     switch (mode) {
@@ -16,6 +29,9 @@ function changeMode(mode) {
             break;
         case "strict":
             background.strictMode();
+            break;
+        case "userset":
+            background.userSetMode();
             break;
         default:
             background.flexibleMode();
@@ -26,11 +42,140 @@ function resetMap(){
  background.resetMap();
 }
 
+// this is the messy function I made that lists the users installed Extensions
+// with checkboxes for use in adding websites for user set mode
+function listExtensions(extensions){
+  // deep copy so the page can be refreshed - makes testing easier
+  items = JSON.parse(JSON.stringify(extensions))
+  // just get the extension names
+  for(i=0; i < items.length; i++){
+    items[i] = items[i]["name"]
+  }
+  // a bunch of code to create HTML elements. I don't understand it super well
+  // because I just copied it from stack overflow
+   // Make a container element for the list
+    listContainer = document.createElement('div'),
+    // Make the list
+    listElement = document.createElement('ul');
+    listElement.id = "extensionList";
+    // listElement.type = "checkbox"
+    // Set up a loop that goes through the items in listItems one at a time
+    numberOfListItems = items.length,
+    // listItem,
+    // i;
+
+    // Add it to the page - bottom i guess?
+    document.getElementsByTagName('body')[0].appendChild(listContainer);
+    listContainer.appendChild(listElement);
+    // actual value creation for list population
+    for (i = 0; i < numberOfListItems; ++i) {
+      // used to set the value to the name of the extension
+      // can't just set a value for a checkbox, it has to be given separately
+        var description = document.createTextNode(items[i])
+        // newline between extensions
+        var brk = document.createElement("br")
+
+        // create an item for each one - makes it a checkbox input
+        listItem = document.createElement('input');
+        listItem.type = "checkbox"
+
+        // Add the item text
+        listItem.name = items[i]
+        listItem.value = items[i]
+        listItem.id = items[i]
+        // listItem.innerHTML = items[i];
+
+        // Add listItem to the listElement
+        // adds checkbox -> extension name -> newline
+        listElement.appendChild(listItem);
+        listElement.appendChild(description);
+        listElement.appendChild(brk);
+    }
+}
+
+function test(){
+  // console.log("test")
+  // console.log(userExtensionList)
+  // console.log(document.getElementById("extensionList"));
+  console.log(background.usersetWebsites);
+  console.log(background.websiteMap);
+  console.log(background.mode);
+  // console.log(background.usersetWebsites[0]);
+  // // background.usersetWebsites.push("test.com")
+  // console.log(background.addToUserSetBlacklist("test.com"))
+  // console.log(background.usersetWebsites)
+}
+
+// called when the user clicks on the extension list or one of the checkboxes
+// NOTE: THIS IS ONLY USED TO KEEP TRACK OF WHICH EXTENSIONS ARE CURRENTLY
+// CHECKED OR UNCHECKED. IT DOES NOT MODIFY THE SETTINGS OF THE WEBSITE AT ALL
+function modifyUserExtensionList(){
+  // console.log(event)
+  // target is supposed to make it so it knows which box is being checked I think
+  var target = event.target || event.srcElement;
+  // if it's not "extensionList", then it's one lf the checkboxes
+  if(event.target.id != "extensionList"){
+    // gets the id of the checkbox (NOT EXTENSION) - same as name I'm pretty sure
+    var item = document.getElementById(event.target.id)
+    // gets the actual ID of the extension - the big long one that looks like
+    //  a hash or something
+    var extensionID = getExtensionID(event.target.id)
+    // checks to see if this extension has been added to our user's list already
+    // (this variable is local to this file)
+    var extensionIndex = userExtensionList.indexOf(extensionID);
+    // if the index is -1, it doesn't exist. add it to the list of checked extensions
+    if(item.checked && extensionIndex == -1){
+        userExtensionList.push(getExtensionID(event.target.id))
+    }
+    // if it is already in the list, remove it when it's unchcked
+    else if(!item.checked && extensionIndex > -1){
+      userExtensionList.splice(extensionIndex,1);
+    }
+  }
+}
+
+// returns the actual extension ID given the extension name
+function getExtensionID(name){
+  for(i=0; i < background.extensionsList.length; i++){
+    if(background.extensionsList[i]['name'] == name){
+      return background.extensionsList[i]['id'];
+    }
+  }
+}
+
+function addToUserSetBlacklist(){
+  // get the value entered by the user when they click the add button
+  url = document.getElementById("urlbox").value;
+  // reset the box
+  document.getElementById("urlbox").value = "";
+  // console.log(url);
+  // add the website and selected extensions to the user blacklist
+  background.addToUserSetBlacklist(url, userExtensionList);
+}
+
+// same as previous function. Doesn't require user extension list because
+// we delete based on url, so all we need is the url to delete
+function removeFromUserSetBlacklist(){
+  url = document.getElementById("urlbox").value;
+  document.getElementById("urlbox").value = "";
+  background.removeFromUserSetBlacklist(url);
+}
+
+function resetUserBlacklist(){
+  background.resetUserBlacklist();
+}
+
+
 document.addEventListener('DOMContentLoaded', restore_options);
 
 document.getElementById("reset").addEventListener("click", function( event ) {
  resetMap();
 }, false);
+
+// on click listeners for the add/remove/reset buttons
+document.getElementById("addurl").addEventListener("click", addToUserSetBlacklist)
+document.getElementById("removeurl").addEventListener("click", removeFromUserSetBlacklist)
+document.getElementById("reseturl").addEventListener("click", resetUserBlacklist)
 
 document.getElementById("selectMode").addEventListener("change", function( event ) {
  console.log("POUET");
